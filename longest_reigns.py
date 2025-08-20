9e2a626b980f64b0fc2bd65dc4319aa723bb9a1d
Kostiantyn Perun <kosperun@gmail.com>
2025-08-20 19:18:04 -0400

    Add longest_reign animation

--- longest_reigns.py
+++ longest_reigns.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import bar_chart_race as bcr
+import math
 
 
 def to_float_year(datestr: str) -> float:
@@ -21,23 +22,39 @@ records = []
 for t in timeline:
     df["tenure_at_t"] = np.clip(t - df["start_f"], 0, df["end_f"] - df["start_f"])
     snapshot = (
-        df.set_index("name_full")["tenure_at_t"].sort_values(ascending=False).head(10)
+        df.set_index("name_full")["tenure_at_t"].sort_values(ascending=False).head(20)
     )
     records.append(snapshot)
 
 race_df = pd.DataFrame(records, index=timeline).fillna(0)
 
+# ----------------------------
+# Extend final frame by ~5s
+# ----------------------------
+period_length = 100
+
+extra_periods = math.ceil(5000 / period_length)  # number of extra periods to add
+last_row = race_df.iloc[[-1]]
+last_rows = pd.concat([last_row] * extra_periods, ignore_index=False)
+
+race_df = pd.concat([race_df, last_rows])
+
+# ----------------------------
+# Animation
+# ----------------------------
 bcr.bar_chart_race(
     df=race_df,
     filename="papal_reigns.mp4",
     orientation="h",
     sort="desc",
-    n_bars=10,
+    n_bars=20,
     fixed_order=False,
     fixed_max=True,
-    steps_per_period=20,
+    steps_per_period=10,
     interpolate_period=False,
-    period_length=70,
-    title="Longest Papal Reigns in History",
+    period_length=period_length,
+    title="20 Longest Papal Reigns in History",
     bar_size=0.95,
+    filter_column_colors=True,
+    period_template="{x:.0f}",
 )
